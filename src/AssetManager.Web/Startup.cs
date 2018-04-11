bb0b985ca4a045ba5f52840da28f161de2f35017
Ratan Sunder Parai <ratanparai@gmail.com>
2018-04-11 19:37:56 +0600

    load different configuration for different environment

--- src/AssetManager.Web/Startup.cs
+++ src/AssetManager.Web/Startup.cs
@@ -18,10 +18,47 @@ namespace AssetManager.Web
 
         public IConfiguration Configuration { get; }
 
+        // This method gets called when Development Environment is used
+        // Use this method to set Development services, like Development database
+        public void ConfigureDevelopmentServices(IServiceCollection services)
+        {
+            // use in-memeory databses
+            ConfigureTestingServices(services);
+
+            // use real database
+            // ConfigureProductionService(services);
+        }
+
+        // This method gets called when Testing Environment is used
+        // Use this method to set Testing services, like Testng database
+        public void ConfigureTestingServices(IServiceCollection services)
+        {
+            // Configure in-memory database
+            //services.AddDbContext<AcademiaContext>(options =>
+            //    options.UseInMemoryDatabase("academia"));
+
+            //ConfigureServices(services);
+        }
+
+        // This method gets called when Production Environment is used
+        // Use this method to set Production services, like Production database
+        public void ConfigureProductionServices(IServiceCollection services)
+        {
+            //services.AddDbContext<AcademiaContext>(options =>
+            //    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            //);
+
+            //ConfigureServices(services);
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+            //services.AddScoped(typeof(IAsyncRepository<>), typeof(EfRepository<>));
+
             services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
