74d4df809760a1e042632905812df9ebb8e145f8
Md. Aftab Uddin Kajal <kajal.alpha@gmail.com>
2018-06-04 19:34:15 +0600

    Software Licenses create user interface added with action method

--- src/AssetManager.Web/Controllers/LicensesController.cs
+++ src/AssetManager.Web/Controllers/LicensesController.cs
@@ -12,10 +12,27 @@ namespace AssetManager.Web.Controllers
     public class LicensesController : Controller
     {
         private readonly IAsyncRepository<License> _licenseasyncRepository;
+        private readonly IAsyncRepository<Company> _companyRepository;
+        private readonly IAsyncRepository<Category> _categoryRepository;
+        private readonly IAsyncRepository<Manufacturer> _menurepository;
+        private readonly IAsyncRepository<Depreciation> _depreciationrepository;
+        public readonly IAsyncRepository<Supplier> _supplierasyncRepository;
 
-        public LicensesController(IAsyncRepository<License> licenseasyncRepository)
+        public LicensesController(
+            IAsyncRepository<Company> companyRepository,
+            IAsyncRepository<Category> categoryRepository,
+            IAsyncRepository<License> licenseasyncRepository,
+            IAsyncRepository<Manufacturer> manufacturer,
+            IAsyncRepository<Depreciation> deprepos,
+            IAsyncRepository<Supplier> supplierRepository
+            )
         {
+            this._companyRepository = companyRepository;
+            this._categoryRepository = categoryRepository;
             this._licenseasyncRepository = licenseasyncRepository;
+            this._menurepository = manufacturer;
+            this._depreciationrepository = deprepos;
+            this._supplierasyncRepository = supplierRepository;
         }
         public async Task<IActionResult> Index()
         {
@@ -23,6 +40,29 @@ namespace AssetManager.Web.Controllers
             return View(licenseslist);
         }
 
-       
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.CompanyList = await _companyRepository.ListAllAsync();
+            ViewBag.Categorylist = await _categoryRepository.ListAllAsync();
+            ViewBag.Manufacturers = await _menurepository.ListAllAsync();
+            ViewBag.Depreciations = await _depreciationrepository.ListAllAsync();
+            ViewBag.Suppliers = await _supplierasyncRepository.ListAllAsync();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(License licenses)
+        {
+            licenses.Depreciate = true;
+            if (ModelState.IsValid)
+            {
+                await _licenseasyncRepository.AddAsync(licenses);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
     }
 }
