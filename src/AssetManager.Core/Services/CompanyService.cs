bf4824d71b82da87e1123e6bb81e2da8209036d1
Md. Aftab Uddin Kajal <kajal.alpha@gmail.com>
2018-07-11 13:02:21 +0600

    Service added

---
+++ src/AssetManager.Core/Services/CompanyService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssetManager.Core.Services
+{
+   public class CompanyService
+    {
+        public CompanyService()
+        {
+
+        }
+
+    }
+}
