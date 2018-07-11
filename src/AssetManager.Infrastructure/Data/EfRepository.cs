2b5b6df98be1ad612ba2ec0c4fe99e995c4be382
Md. Aftab Uddin Kajal <kajal.alpha@gmail.com>
2018-07-11 15:30:05 +0600

    IRepository Implemented and  Efrepository modified

--- src/AssetManager.Infrastructure/Data/EfRepository.cs
+++ src/AssetManager.Infrastructure/Data/EfRepository.cs
@@ -3,18 +3,26 @@ using AssetManager.Core.SharedKernel;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AssetManager.Infrastructure.Data
 {
-    public class EfRepository<T> : IAsyncRepository<T> where T : Entity
+    public class EfRepository<T> : IRepository<T>, IAsyncRepository<T> where T : Entity
     {
         private readonly AssetManagerContext _dbContext;
 
         public EfRepository(AssetManagerContext dbContext)
         {
             this._dbContext = dbContext;
+        }
+
+        public T Add(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+            _dbContext.SaveChanges();
+            return entity;
         }
 
         public async Task<T> AddAsync(T entity)
@@ -24,10 +32,21 @@ namespace AssetManager.Infrastructure.Data
             return entity;
         }
 
+        public void Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            _dbContext.SaveChanges();
+        }
+
         public async Task DeleteAsync(T entity)
         {
             _dbContext.Set<T>().Remove(entity);
             await _dbContext.SaveChangesAsync();
+        }
+
+        public T GetById(int id)
+        {
+            return _dbContext.Set<T>().Find(id);
         }
 
         public async Task<T> GetByIdAsync(int id)
@@ -36,9 +55,30 @@ namespace AssetManager.Infrastructure.Data
             return await _dbContext.Set<T>().SingleOrDefaultAsync(m => m.Id == id);
         }
 
+        public T GetSingleBySpec(ISpecification<T> spec)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> List(ISpecification<T> spec)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> ListAll()
+        {
+            return _dbContext.Set<T>().AsEnumerable();
+        }
+
         public async Task<List<T>> ListAllAsync()
         {
             return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public void Update(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            _dbContext.SaveChanges();
         }
 
         public async Task UpdateAsync(T entity)
