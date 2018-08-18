0da6511582c06203fc098ce9e6c90f5b93506a7f
Md. Aftab Uddin Kajal <kajal.alpha@gmail.com>
2018-08-18 22:53:53 +0600

    Category vm added

---
+++ src/AssetManager.Web/Service/CategoryViewModelService.cs
@@ -0,0 +1,12 @@
+﻿using AssetManager.Web.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetManager.Web.Service
+{
+    public class CategoryViewModelService : ICategoryViewModelService
+    {
+    }
+}
---
+++ src/AssetManager.Web/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetManager.Web.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = ("Catagory Name"))]
+        public string Name { get; set; }
+
+        [Display(Name = ("EULA"))]
+        public string EulaText { get; set; }
+
+        [Display(Name = ("Use the primary default EULA instead. "))]
+        public bool DefaultEula { get; set; }
+
+        [Display(Name = "Category Type")]
+        public CatagoryTypeVM Type { get; set; }
+
+        [Display(Name = ("Send email to user on checkin."))]
+        public bool CheckInEmail { get; set; }
+
+        [Display(Name = ("Image"))]
+        public string Image { get; set; }
+    }
+    public enum CatagoryTypeVM
+    {
+        [Display(Name = "Asset")]
+        Asset,
+        [Display(Name = "Component")]
+        Component,
+        [Display(Name = "Accessory")]
+        Accessory,
+        [Display(Name = "Consumable")]
+        Consumable
+    }
+}
